b5410d6dd87352c8aeff569ea0c8afdb41804868
Bablu Ahmed <bablukpik@gmail.com>
2023-01-23 18:30:45 +0600

    main: icons example added for the record

--- utilities/record.ts
+++ utilities/record.ts
@@ -2,8 +2,15 @@
  * Example 1
  */
 
+// type ThreeStringProps = { [key: string]: string };
 // type ThreeStringProps = { prop1: string, prop2: string, prop3: string }
 type ThreeStringProps = Record<'prop1' | 'prop2' | 'prop3', string>
+
+const obj: ThreeStringProps = {
+  prop1: 'foo',
+  prop2: 'bar',
+  prop3: 'asdf',
+};
 
 /*
 * Example 2
@@ -21,3 +28,34 @@ const cats: Record<CatName, CatInfo> = {
   boris: { age: 5, breed: 'Maine Coon' },
   mordred: { age: 16, breed: 'British Shorthair' },
 };
+
+/*
+ * Example 3
+ */
+
+type Statuses = 'failed' | 'complete';
+type IconTypes = 'warning' | 'check';
+type IconColors = 'red' | 'green';
+
+interface Status {
+  iconType: IconTypes;
+  iconColor: IconColors;
+}
+
+type IconsType = Record<
+  Statuses,
+  Status
+>;
+
+const icons: IconsType = {
+  failed: {
+    iconType: 'warning',
+    iconColor: 'red',
+  },
+  complete: {
+    iconType: 'check',
+    iconColor: 'green',
+  },
+};
+
+// const Status = (status: Statuses) => <Icon {...icons[status]}/>
